ad92da9ef32a5684ed38893565d654f01168fb84
Adria Blancafort <adriablancafort@gmail.com>
2024-06-15 13:42:34 +0200

    modal before delete from cart

--- server.py
+++ server.py
@@ -28,11 +28,21 @@ async def server(websocket, path):
                     product['quantity'] = product_quantity
                     cart.append(product)
                 await websocket.send(json.dumps({'action': 'load_cart', 'cart': cart}))
+
         elif action == 'remove_from_cart':
             product_id = int(data.get('id'))
             cart_item = next((item for item in cart if item['id'] == product_id), None)
             if cart_item:
                 cart.remove(cart_item)
+                await websocket.send(json.dumps({'action': 'load_cart', 'cart': cart}))
+
+        elif action == 'change_quantity':
+            product_id, change = int(data.get('id')), int(data.get('quantity'))
+            cart_item = next((item for item in cart if item['id'] == product_id), None)
+            if cart_item:
+                cart_item['quantity'] += change
+                if cart_item['quantity'] <= 0:
+                    cart.remove(cart_item)
                 await websocket.send(json.dumps({'action': 'load_cart', 'cart': cart}))
 
 start_server = websockets.serve(server, "0.0.0.0", 8765)
